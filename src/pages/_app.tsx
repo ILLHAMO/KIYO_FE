848a0e2ded525b8b017ac44daa4a56780db1939d
Kim Jina <71418418+wlsdk7245@users.noreply.github.com>
2022-05-12 22:35:43 +0900

    Revert "[FE] 마이페이지 퍼블리싱 (#11)" (#13)
    This reverts commit 09c60233e5657314a3f3e32545ee05452dba4ea2.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import type { AppProps } from 'next/app';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
