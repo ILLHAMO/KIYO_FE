f6e1835c068cb6cad52a6db96ae8d8141da8f272
김진아 <wlsdk7245@gmail.com>
2022-05-13 10:25:21 +0900

    feat: 북마크 리스트 없음 퍼블리싱

--- src/pages/bookmark/index.tsx
+++ src/pages/bookmark/index.tsx
@@ -1,13 +1,21 @@
-import BookmarkList from 'app.feature/bookmark/BookmarkList';
 import React from 'react';
 import styled from 'styled-components';
+import BookmarkList from 'app.feature/bookmark/BookmarkList';
+import BookmarkNone from 'app.feature/bookmark/BookmarkNone';
+import NavigationBar from 'app.components/NavigationBar/NavigationBar';
 
 const PageBookmark = () => {
-  return <StyledWrapper>
-    <BookmarkList />
-  </StyledWrapper>;
+  return (
+    <StyledWrapper>
+      {/*<BookmarkList />*/}
+      <BookmarkNone />
+      <NavigationBar />
+    </StyledWrapper>
+  );
 };
 
 export default PageBookmark;
 
-const StyledWrapper = styled.div``;
+const StyledWrapper = styled.div`
+  height: 100vh;
+`;
