26546f3ee119391a5e0a21232f951030e9a51ce2
HWI <92978741+oxhwixo@users.noreply.github.com>
2022-05-15 12:50:30 +0900

    feat: 등록 모달, 장소 등록 페이지, 장소 등록 완료 페이지 퍼블리싱 (#22)

---
+++ src/app.feature/register/RegisterComplete.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const RegisterComplete = () => {
+  return (
+    <StyledWrapper>
+			<img src="/images/register/logo.png" alt="" />
+			<div className="big-ment">감사합니다 :)</div>
+			<div className="small-ment">검토 후 장소 등록이 완료됩니다.</div>
+			<img src="/images/register/background.png" alt="" />
+			<img src="/images/register/tokkis.png" alt="" />
+			<div className="button">키즈존 찾으러 가기</div>
+    </StyledWrapper>
+  );
+};
+
+export default RegisterComplete;
+
+const StyledWrapper = styled.div`
+	padding: 56px 48px;
+
+	img:nth-child(1) {
+		width: 80px;
+		height: 40px;
+	}
+	img:nth-child(4) {
+		position: absolute;
+		top: 58%;
+  	left: 50%;
+  	transform: translate(-50%, -50%);
+	}
+	img:nth-child(5) {
+		position: absolute;
+		width: 192px;
+		height: 210.24px;
+		top: 58%;
+  	left: 50%;
+  	transform: translate(-50%, -50%);
+	}
+
+	.big-ment {
+		margin: 32px 0 8px 0;
+		font-size: 30px;
+		color: #555555;
+		font-weight: bold;
+	}
+
+	.small-ment {
+		color: #555555;
+	}
+
+	.button {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		color: white;
+		position: absolute;
+		width: 215px;
+		height: 46px;
+		top: 87%;
+  	left: 50%;
+  	transform: translate(-50%, -50%);
+		background: #85C07B;
+		border-radius: 25px;
+	}
+`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import HomeBanner from 'app.feature/home/HomeBanner';
 import HomeStoreFilter from 'app.feature/home/HomeStoreFilter';
 import HomeStoreList from 'app.feature/home/HomeStoreList';
 import HomeConvenienceFilter from 'app.feature/home/ HomeConvenienceFilter';
+import RegisterModal from 'app.feature/register/RegisterModal';
 
 const PageHome = () => {
   return (
@@ -15,6 +16,7 @@ const PageHome = () => {
       <HomeStoreFilter />
       <HomeStoreList />
       {/* <HomeConvenienceFilter /> */}
+      {/* <RegisterModal /> */}
     </StyledWrapper>
   );
 };
