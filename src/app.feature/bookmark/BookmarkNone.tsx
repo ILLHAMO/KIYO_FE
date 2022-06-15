12f6b11c7f707fbdad3b2de22047e021a0252b21
김진아 <wlsdk7245@gmail.com>
2022-06-15 22:54:00 +0900

    css: 북마크 페이지 BEM 수정

--- src/app.feature/bookmark/BookmarkNone.tsx
+++ src/app.feature/bookmark/BookmarkNone.tsx
@@ -3,9 +3,9 @@ import styled from 'styled-components';
 
 const BookmarkNone = () => {
   return (
-    <StyledWrapper>
-      <div className='none-text'>북마크한 키즈존이 없습니다.</div>
-      <div className='add-button'>북마크하러 가기</div>
+    <StyledWrapper className="bookmark-none">
+      <div className="bookmark-none__text">북마크한 키즈존이 없습니다.</div>
+      <div className="bookmark-none__add-button">북마크하러 가기</div>
     </StyledWrapper>
   );
 };
@@ -18,15 +18,15 @@ const StyledWrapper = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  
-  .none-text {
+
+  .bookmark-none__text {
     font-weight: 700;
     font-size: 18px;
     line-height: 26px;
     margin-bottom: 20px;
   }
-  
-  .add-button {
+
+  .bookmark-none__add-button {
     height: 48px;
     padding: 0 32px;
     line-height: 48px;
