e044c434c172dece659a7fdb40a8c2e5206d3241
jina <wlsdk7245@gmail.com>
2022-06-30 17:30:45 +0900

    feat: react-hook-form 추가, 사진 upload 추가

--- src/app.feature/register/RegisterForm.tsx
+++ src/app.feature/register/RegisterForm.tsx
@@ -1,196 +1,243 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import FilterConvenience from 'app.components/Filter/FilterConvenience';
 import FilterCategory from 'app.components/Filter/FilterCategory';
+import { useForm, FormProvider } from 'react-hook-form';
+import { message, Upload, UploadProps } from 'antd';
 
 const RegisterForm = () => {
-  return (
-    <StyledWrapper className='register-form'>
-			<div className="register-form__body">
-				<div className="register-form__big-title">기본 정보</div>
-				<div className="register-form__input">
-					<div className="register-form__small-title">식당 이름 :</div>
-					<input type="text" />
-				</div>
-				<div className="register-form__input">
-					<div className="register-form__small-title">주소 :</div>
-					<input type="text" />
-				</div>
-				<div className="register-form__input">
-					<div className="register-form__small-title">운영 방식 :</div>
-					<div className="register-form__radio">
-						<input type="radio" name="키즈존 여부"/>
-						<div className="register-form__radio-title">키즈존</div>
-						<input type="radio" name="키즈존 여부"/>
-						<div className="register-form__radio-title">일반식당</div>
-					</div>
-				</div>
+  const [fileList, setFileList] = useState([]);
 
-				<div className="register-form__big-title">추가 정보</div>
-				<div className="register-form__input">
-					<div className="register-form__small-title">전화번호 :</div>
-					<input type="text" />
-				</div>
-				<div className="review-photo-wrap">
-					<div className="title">사진 추가하기</div>
-					<div className="review-photo-slide">
-						<div className="photo-item"></div>
-						<div className="photo-item"></div>
-						<div className="photo-item"></div>
-						<div className="photo-item"></div>
-						<div className="photo-item"></div>
-						<div className="photo-item"></div>
-						<div className="photo-item"></div>
-						<div className="photo-item"></div>
-					</div>
-					<div className="review-warn">
-						<li>
-							가게 내/외부,음식,메뉴판 등 사진을 추가해주세요.
-						</li>
-						<li>사진은 10개까지 등록하실 수 있습니다.</li>
-					</div>
-				</div>
-				<div className="register-form__filter">
-					<div className="register-form__filter-title">
-						장소 종류 선택
-					</div>
-					<FilterCategory />
-				</div>
-				<div className="register-form__filter">
-					<div className="register-form__filter-title">
-						편의 사항 선택
-					</div>
-			 		<FilterConvenience />
-				</div>
-				<div className="register-form__notice">· 검토 후 장소 등록이 완료됩니다.</div>
-			</div>
-      <div className='register-form__button'>장소 등록하기</div>
-    </StyledWrapper>
+  const methods = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = methods;
+
+  const handleChange: UploadProps['onChange'] = ({ fileList: newFileList }) =>
+    setFileList(newFileList);
+
+  const onValidRegisterForm = (data) => {
+    try {
+      data.name = data.name.trim();
+      data.address = data.address.trim();
+
+      if (!data.name || !data.address || !data.isKidsZone)
+        message.error('기본 정보를 입력해주세요!');
+
+      // 등록 API
+      console.log('등록하기');
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const onInvalidRegisterForm = (errors) => {
+    Object.keys(errors).map((item) => {
+      message.error(errors[item].message);
+    });
+  };
+
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={handleSubmit(onValidRegisterForm, onInvalidRegisterForm)}>
+        <StyledWrapper className="register-form">
+          <div className="register-form__body">
+            <div className="register-form__big-title">기본 정보</div>
+            <div className="register-form__input">
+              <div className="register-form__small-title">식당 이름 :</div>
+              <input
+                {...register('name', {
+                  // required: '식당 이름은 필수입니다!',
+                  maxLength: {
+                    value: 20,
+                    message: '식당 이름은 20자 이내로 작성해주세요!',
+                  },
+                })}
+                type="text"
+              />
+            </div>
+            <div className="register-form__input">
+              <div className="register-form__small-title">주소 :</div>
+              <input
+                {...register('address', {
+                  // required: '주소는 필수입니다!',
+                })}
+                type="text"
+              />
+            </div>
+            <div className="register-form__input">
+              <div className="register-form__small-title">운영 방식 :</div>
+              <div className="register-form__radio">
+                <input {...register('isKidsZone')} type="radio" value="true" />
+                <div className="register-form__radio-title">키즈존</div>
+                <input {...register('isKidsZone')} type="radio" value="false" />
+                <div className="register-form__radio-title">일반식당</div>
+              </div>
+            </div>
+            <div className="register-form__big-title">추가 정보</div>
+            <div className="register-form__input">
+              <div className="register-form__small-title">전화번호 :</div>
+              <input type="text" />
+            </div>
+            <div className="review-photo-wrap">
+              <div className="title">사진 추가하기</div>
+
+              <Upload
+                {...register('image')}
+                listType="picture-card"
+                fileList={fileList}
+                // onPreview={handlePreview}
+                onChange={handleChange}
+                className="review-photo-slide"
+              >
+                <div>
+                  +<br />
+                  <div style={{ marginTop: 8 }}>Upload</div>
+                </div>
+              </Upload>
+              <div className="review-warn">
+                <li>가게 내/외부,음식,메뉴판 등 사진을 추가해주세요.</li>
+                <li>사진은 10개까지 등록하실 수 있습니다.</li>
+              </div>
+            </div>
+            <div className="register-form__filter">
+              <div className="register-form__filter-title">장소 종류 선택</div>
+              <FilterCategory />
+            </div>
+            <div className="register-form__filter">
+              <div className="register-form__filter-title">편의 사항 선택</div>
+              <FilterConvenience />
+            </div>
+            <div className="register-form__notice">
+              · 검토 후 장소 등록이 완료됩니다.
+            </div>
+          </div>
+          <button type="submit" className="register-form__button">
+            장소 등록하기
+          </button>
+        </StyledWrapper>
+      </form>
+    </FormProvider>
   );
 };
 
 export default RegisterForm;
 
 const StyledWrapper = styled.div`
+  .register-form__body {
+    padding-bottom: 72px;
 
-	.register-form__body {
-		margin-bottom: 72px;
+    .register-form__big-title {
+      padding: 0 20px 0;
+      display: flex;
+      align-items: center;
+      width: 100%;
+      height: 50px;
+      font-size: 24px;
+      border-bottom: 0.5px solid var(--color-gray-100);
+    }
 
-		.register-form__big-title {
-			padding: 0 20px 0;
-			display: flex;
-			align-items: center;
-			width: 100%;
-			height: 50px;
-			font-size: 24px;
-			border-bottom: 0.5px solid var(--color-gray-100);
-		}
+    .register-form__input {
+      width: 90%;
+      height: 50px;
+      margin-left: 5%;
+      display: flex;
+      align-items: center;
+      border-bottom: 0.5px solid var(--color-gray-100);
 
-		.register-form__input {
-			width: 90%;
-			height: 50px;
-			margin-left: 5%;
-			display: flex;
-			align-items: center;
-			border-bottom: 0.5px solid var(--color-gray-100);
-	
-			.register-form__small-title {
-				flex-shrink: 0;
-			}
-	
-			input {
-				width: 100%;
-				margin-left: 4px;
-				caret-color: var(--color-main);
-			}
-	
-			.register-form__radio {
-				margin: 0 16px 0;
-				display: grid;
-				grid-template-columns: repeat(4, auto);
-				grid-gap: 10px;
-				justify-content: center;
-				align-items: center;
-	
-				@media only screen and (max-width: 325px) {
-					grid-gap: 0px;
-					word-break: keep-all;
-				}
-	
-				input {
-					width: 24px;
-					margin: 0;
-				}
-	
-				.register-form__radio-title {
-					margin-right: 8px;
-				}
-			}
-		}
+      .register-form__small-title {
+        flex-shrink: 0;
+      }
 
-		.register-form__notice {
-			padding-left: 20px;
-			color: var(--color-gray-300);
-			font-size: 12px;
-		}
+      input {
+        width: 100%;
+        margin-left: 4px;
+        caret-color: var(--color-main);
+      }
 
-		.review-photo-wrap {
-			padding: 0 20px 0;
-			margin-bottom: 26px;
-	
-			.title {
-				margin-top: 16px;
-				margin-bottom: 8px;
-				font-size: 16px;
-				line-height: 23px;
-			}
-	
-			.review-photo-slide {
-				display: flex;
-				align-items: center;
-				overflow: auto;
-				margin: 0 -20px 8px;
-				padding: 0 20px;
-	
-				.photo-item {
-					min-width: 80px;
-					border: 1px solid var(--color-black);
-					border-right: none;
-					width: 80px;
-					height: 80px;
-					background: #ffe9ef;
-	
-					&:last-child {
-						border-right: 1px solid var(--color-black);
-					}
-				}
-			}
-	
-			.review-warn {
-				font-size: 12px;
-				line-height: 17px;
-				color: var(--color-gray-300);
-			}
-		}
+      .register-form__radio {
+        margin: 0 16px 0;
+        display: grid;
+        grid-template-columns: repeat(4, auto);
+        grid-gap: 10px;
+        justify-content: center;
+        align-items: center;
 
-		.register-form__filter {
-			width: 90%;
-			margin-left: 5%;
-			margin-bottom: 24px;
-			display: flex;
-			flex-direction: column;
-			justify-content: center;
-			border-top: 0.5px solid var(--color-gray-100);
-	
-			.register-form__filter-title {
-				margin: 24px 0;
-			}
-	
-			.filter-wrap {
-				padding: 0 20px 0;
-			}
-		}
-	}
+        @media only screen and (max-width: 325px) {
+          grid-gap: 0px;
+          word-break: keep-all;
+        }
+
+        input {
+          width: 24px;
+          margin: 0;
+        }
+
+        .register-form__radio-title {
+          margin-right: 8px;
+        }
+      }
+    }
+
+    .register-form__notice {
+      padding-left: 20px;
+      color: var(--color-gray-300);
+      font-size: 12px;
+    }
+
+    .review-photo-wrap {
+      padding: 0 20px 0;
+      margin-bottom: 26px;
+
+      .title {
+        margin-top: 16px;
+        margin-bottom: 8px;
+        font-size: 16px;
+        line-height: 23px;
+      }
+
+      .review-photo-slide {
+        .ant-upload-list {
+          display: flex;
+          overflow: auto;
+          flex-flow: row-reverse;
+          justify-content: flex-end;
+        }
+
+        .ant-upload-list-picture-card-container,
+        .ant-upload {
+          flex-shrink: 0;
+        }
+      }
+
+      .review-warn {
+        font-size: 12px;
+        line-height: 17px;
+        color: var(--color-gray-300);
+      }
+    }
+
+    .register-form__filter {
+      width: 90%;
+      margin-left: 5%;
+      margin-bottom: 24px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      border-top: 0.5px solid var(--color-gray-100);
+
+      .register-form__filter-title {
+        margin: 24px 0;
+      }
+
+      .filter-wrap {
+        padding: 0 20px 0;
+      }
+    }
+  }
 
   .register-form__button {
     cursor: pointer;
@@ -206,5 +253,5 @@ const StyledWrapper = styled.div`
     color: var(--color-white);
     font-weight: 500;
     font-size: 18px;
-  } 
+  }
 `;
