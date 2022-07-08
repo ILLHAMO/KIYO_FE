c1d7f5fca4c6e29e410f07dd6e1c8c526a1c410a
jina <wlsdk7245@gmail.com>
2022-07-08 12:40:15 +0900

    fix: ENDPOINT 변경

--- src/app.feature/enter/EnterLoginForm.tsx
+++ src/app.feature/enter/EnterLoginForm.tsx
@@ -1,30 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
-import axios from 'axios';
 import Link from 'next/link';
 
 const EnterLoginForm = () => {
-  const handleLogin = async (snsType: string) => {
-    try {
-      await axios.post('');
-    } catch (err) {}
-  };
-
   return (
     <StyledWrapper className="enter-login-form">
       <div className="enter-login-form__text">SNS 아이디로 로그인하기</div>
       <div className="enter-login-form__login-method">
-        <Link href="http://localhost:8080/oauth2/authorization/kakao?redirect_uri=http://localhost:3000/oauth/redirect">
+        <Link href="https://www.jmsteady.net/oauth2/authorization/kakao?redirect_uri=https://gotokidszone.netlify.app/oauth/redirect">
           <div className="enter-login-form__method enter-login-form__method--kakao">
             <img src="/images/enter/kakao.png" alt="kakao" />
           </div>
         </Link>
-        <Link href="http://localhost:8080/oauth2/authorization/google?redirect_uri=http://localhost:3000/oauth/redirect">
+        <Link href="https://www.jmsteady.net/oauth2/authorization/google?redirect_uri=https://gotokidszone.netlify.app/oauth/redirect">
           <div className="enter-login-form__method enter-login-form__method--google">
             <img src="/images/enter/google.png" alt="google" />
           </div>
         </Link>
-        <Link href="http://localhost:8080/oauth2/authorization/naver?redirect_uri=http://localhost:3000/oauth/redirect">
+        <Link href="https://www.jmsteady.net/oauth2/authorization/naver?redirect_uri=https://gotokidszone.netlify.app/oauth/redirect">
           <div className="enter-login-form__method enter-login-form__method--naver">
             <img src="/images/enter/naver.png" alt="naver" />
           </div>
